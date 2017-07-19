16a8b74286d2de5a8f5b1a6ed9bc4d01c0f95828
Thiago Nogueira <thiago.droz@gmail.com>
2017-07-19 00:28:05 -0300

    Creating new tiles

--- racing/js/imageLoading.js
+++ racing/js/imageLoading.js
@@ -1,6 +1,9 @@
 var roadPic = document.createElement("img");
 var wallPic = document.createElement("img");
 var carPic = document.createElement("img");
+var goalPic = document.createElement("img");
+var treePic = document.createElement("img");
+var flagPic = document.createElement("img");
 
 var picsToLoad = 0;
 
@@ -21,7 +24,10 @@ function loadImages() {
     var imageList = [
         { imgVar: carPic, filePath: "./images/player1car.png" },
         { imgVar: roadPic, filePath: "./images/track_road.png" },
-        { imgVar: wallPic, filePath: "./images/track_wall.png" }
+        { imgVar: wallPic, filePath: "./images/track_wall.png" },
+        { imgVar: goalPic, filePath: "./images/track_goal.png" },
+        { imgVar: treePic, filePath: "./images/track_tree.png" },
+        { imgVar: flagPic, filePath: "./images/track_flag.png" }
     ];
 
     picsToLoad = imageList.length;
--- racing/js/track.js
+++ racing/js/track.js
@@ -2,30 +2,33 @@ const TRACK_W = 40;
 const TRACK_H = 40;
 const TRACK_COLS = 20;
 const TRACK_ROWS = 15;
-var trackGrid = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-                 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+var trackGrid = [4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4,
+                 4, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+                 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
                  1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1,
-                 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1,
-                 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1,
-                 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1,
-                 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
-                 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
-                 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
-                 1, 0, 2, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+                 1, 0, 0, 0, 1, 1, 1, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 0, 0, 1,
+                 1, 0, 0, 1, 1, 0, 0, 1, 4, 4, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1,
+                 1, 0, 0, 1, 0, 0, 0, 0, 1, 4, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1,
+                 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 5, 0, 0, 1, 0, 0, 1,
+                 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+                 1, 0, 0, 1, 0, 0, 5, 0, 0, 0, 5, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+                 1, 0, 2, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 5, 0, 0, 1,
                  1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
-                 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1,
-                 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
-                 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+                 0, 3, 0, 0, 0, 0, 1, 4, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1,
+                 0, 3, 0, 0, 0, 0, 1, 4, 4, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
+                 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 4];
 const TRACK_ROAD = 0;
 const TRACK_WALL = 1;
 const TRACK_PLAYERSTART = 2;
+const TRACK_GOAL = 3;
+const TRACK_TREE = 4;
+const TRACK_FLAG = 5;
 
-function isWallAtColRow(col, row) {
+function isObstacleAtColRow(col, row) {
     if(col >= 0 && col < TRACK_COLS &&
         row >= 0 && row < TRACK_ROWS) {
         var trackIndexUnderCoord = rowColToArrayIndex(col, row);
-        return (trackGrid[trackIndexUnderCoord] == TRACK_WALL);
+        return (trackGrid[trackIndexUnderCoord] != TRACK_ROAD);
     } else {
         return false;
     }
@@ -34,12 +37,11 @@ function isWallAtColRow(col, row) {
 function carTrackHandling() {
     var carTrackCol = Math.floor(carX / TRACK_W);
     var carTrackRow = Math.floor(carY / TRACK_H);
-    var trackIndexUnderCar = rowColToArrayIndex(carTrackCol, carTrackRow);
 
     if(carTrackCol >= 0 && carTrackCol < TRACK_COLS &&
         carTrackRow >= 0 && carTrackRow < TRACK_ROWS) {
 
-        if(isWallAtColRow( carTrackCol,carTrackRow )) {
+        if(isObstacleAtColRow( carTrackCol,carTrackRow )) {
             carX -= Math.cos(carAng) * carSpeed;
             carY -= Math.sin(carAng) * carSpeed;
 
@@ -57,12 +59,28 @@ function drawTracks() {
         for(var eachCol=0;eachCol<TRACK_COLS;eachCol++) {
 
             var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
+            var tileKind = trackGrid[arrayIndex];
+            var useImg;
 
-            if(trackGrid[arrayIndex] == TRACK_WALL) {
-                canvasContext.drawImage(wallPic, TRACK_W*eachCol,TRACK_H*eachRow);
-            } else if(trackGrid[arrayIndex] == TRACK_ROAD) {
-                canvasContext.drawImage(roadPic, TRACK_W*eachCol,TRACK_H*eachRow);
+            switch (tileKind) {
+                case TRACK_WALL:
+                    useImg = wallPic;
+                    break;
+                case TRACK_ROAD:
+                    useImg = roadPic;
+                    break;
+                case TRACK_GOAL:
+                    useImg = goalPic;
+                    break;
+                case TRACK_TREE:
+                    useImg = treePic;
+                    break;
+                case TRACK_FLAG:
+                    useImg = flagPic;
+                    break;
             }
+
+            canvasContext.drawImage(useImg, TRACK_W*eachCol,TRACK_H*eachRow);
         } // end of for each track
     } // end of for each row
 } // end of drawTracks func
